ee97e79687937d5c8d42374e0ce5217607d61753
theChosenDevop <tobbey.adesanya@gmail.com>
2023-07-24 21:08:34 -0700

    handle print function

---
+++ handle_print.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * handle_print - prints argument based on its type
+ * @fmt: formatted strings
+ * @list: variadic list of arguments
+ * @ind: index
+ * @buffer: buffer of array
+ * @flags: calc. active flags
+ * @width: get width
+ * @precision: input
+ * @size: input
+ * Return: 1 or 2
+ */
+
+int handle_print(const char *fmt, int *ind, va_List list, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
+
+	fm_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecima},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+		};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
---
+++ main.h
@@ -0,0 +1,70 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define UNUSED(x) void(x)
+#define BUFF_SIZE 1024
+
+/*** FLAGS ***/
+#define F_MINUS 1
+#define F_plus 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/**Sizes**/
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct fmt - Struct op
+ * @fmt: Format string
+ * @fn: Function associated
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+/**
+ *typdef struct fmt fmt_t - struct op
+ * @fmt: Format
+ * @fm_t: associated function
+ */
+typedef struct fmt fmt_t;
+
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i, va_list, char buffer[], int flags,
+		int width, int precision, int size);
+
+/*** FUnctions ***/
+int print_char(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+int print_percent(va_List types, char buffer[], int flags,
+		int width, int precision, int size);
+
+/** Function to output number **/
+int print_int(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+int print_binary(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+int print_octal(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[], int flags,
+		char flag_ch, int width, int precision, int size);
+
+int print_non_printable(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+
+#endif
