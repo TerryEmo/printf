8c85ace11b9e6bba61dd14f220c505fdea3b57cd
theChosenDevop <tobbey.adesanya@gmail.com>
2023-07-24 21:26:21 -0700

    get width function

---
+++ get_width.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * get_width - calc the width for printing
+ * @format: Formatted string
+ * @i: list of arguments to be printed
+ * @list: variadic function list
+ * Return: width
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+	int j;
+	int width = 0;
+
+	for (j = *i + 1; format[j]; j++)
+	{
+		if (is_digit(format[j]))
+		{
+			width *= 10;
+			width += format[j] - '0';
+		}
+		else if (format[j] == '*')
+		{
+			j++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i =  j;
+
+	return (width);
+}
--- main.h
+++ main.h
@@ -67,4 +67,34 @@ int print_hexa(va_list types, char map_to[], char buffer[], int flags,
 int print_non_printable(va_list types, char buffer[], int flags,
 		int width, int precision, int size);
 
+int print_pointer(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const *format, int *i);
+
+int print_reverse(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+
+int print_rot13string(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+
+int handle_write(char c, char buffer[], int flags,
+		int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[], int flags,
+		int width, int ptrecision, int size);
+int write_pointer(char buffer[], int ind, int length, int width,
+		int flags, char padd, char extra_c, int padd_start);
+int write_unsgd(int is_negative, int ind, char buffer[],
+		int flags, int width, int precision, int size);
+
+int is_printable(char);
+int append_hexa_code(char, char [], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+
 #endif
